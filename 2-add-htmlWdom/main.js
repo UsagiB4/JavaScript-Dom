5508a2c82ed05b2df01c5d247877543ec792e624
UsagiB4 <binaryBunny@tutanota.com>
2022-02-02 00:30:12 +0600

    setAttribute

---
+++ 2-add-htmlWdom/main.js
@@ -0,0 +1,5 @@
+let qrSelect = document.querySelector('h3')  // selecting the first "h3" tag
+console.log(qrSelect)
+
+// setAttribute
+qrSelect.setAttribute('title', 'a title is here')  // setting an attribute to the first "h3" tag
